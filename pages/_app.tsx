1adbf3453cb736accf4d6a34da2242be178543e6
Kyle Nakamura <kylej.nakamura@yahoo.com>
2022-05-12 09:57:57 -0700

    refactor(navbar): move navbar to storybook-static folder
    re #51

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,7 +8,7 @@ import CssBaseline from '@mui/material/CssBaseline'
 import { CacheProvider, EmotionCache } from '@emotion/react'
 import theme from '../src/theme'
 import createEmotionCache from '../src/createEmotionCache'
-import NavBar from '../src/NavBar'
+import NavBar from '../storybook-static/NavBar'
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache()
