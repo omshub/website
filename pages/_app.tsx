c0fdfecdaaa1d3db4ebea183e253713db3ab2cdb
Herman Rogers <herman.rogers@pm.me>
2022-05-12 21:26:20 -0400

    chore(rebase): rebase

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,12 @@
-// pages/_app.tsx
-
-import * as React from 'react'
-import Head from 'next/head'
-import { AppProps } from 'next/app'
-import { ThemeProvider } from '@mui/material/styles'
-import CssBaseline from '@mui/material/CssBaseline'
 import { CacheProvider, EmotionCache } from '@emotion/react'
-import theme from '../src/theme'
+import CssBaseline from '@mui/material/CssBaseline'
+import { ThemeProvider } from '@mui/material/styles'
+import { AppProps } from 'next/app'
+import Head from 'next/head'
+import * as React from 'react'
+import { NavBar } from '../src/components/NavBar'
 import createEmotionCache from '../src/createEmotionCache'
-import { NavBar } from '../src/stories/NavBar'
+import theme from '../src/theme'
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache()
--- src/theme.ts
+++ src/theme.ts
@@ -1,5 +1,5 @@
-import { createTheme } from '@mui/material/styles'
 import { red } from '@mui/material/colors'
+import { createTheme } from '@mui/material/styles'
 
 // Create a theme instance.
 const theme = createTheme({
