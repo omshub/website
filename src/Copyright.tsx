74dacb4979f3b3692e6e8dc68c7393f3ede0a3d8
Herman Rogers <herman.rogers@pm.me>
2022-05-15 19:23:30 -0400

    feat(component): add class card component

--- src/Copyright.tsx
+++ src/Copyright.tsx
@@ -1,14 +1,10 @@
 import * as React from 'react'
 import Typography from '@mui/material/Typography'
-import MuiLink from '@mui/material/Link'
 
 export default function Copyright() {
 	return (
 		<Typography variant='body2' color='text.secondary' align='center'>
-			{'Copyright © '}
-			<MuiLink color='inherit' href='https://mui.com/'>
-				Your Website
-			</MuiLink>{' '}
+			{'Made with ♥ by the GATech community '}
 			{new Date().getFullYear()}.
 		</Typography>
 	)
