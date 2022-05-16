12299e2b7ee78657950495a0623ea8aeeec8997e
Herman Rogers <herman.rogers@pm.me>
2022-05-16 19:16:34 -0400

    chore(rebase): rebase

--- src/stories/ClassCard.stories.tsx
+++ src/stories/ClassCard.stories.tsx
@@ -8,36 +8,26 @@ export default {
 	component: ClassCard,
 } as ComponentMeta<typeof ClassCard>
 
-const Template: ComponentStory<typeof ClassCard> = () => (
+const Template: ComponentStory<typeof ClassCard> = (args) => (
 	<>
 		<Grid container spacing={2}>
 			<Grid item xs={4}>
-				<ClassCard
-					title='Machine Learning for Trading'
-					acronym='ML4T'
-					classId='CS 7641'
-					blurb='This course introduces students to the real world challenges of implementing machine learning based trading strategies including the algorithmic steps from information gathering to market orders.'
-					reviewCount={100}
-					reviewValue={3.5}
-					difficulty={3.0}
-					image='/static/omshub-stub.jpg'
-				></ClassCard>
-			</Grid>
-			<Grid item xs={4}>
-				<ClassCard
-					title='Knowledge Based AI'
-					acronym='KBAI'
-					classId='CS 7637'
-					blurb='The twin goals of knowledge-based artificial intelligence (AI) are to build AI agents capable of human-level intelligence and gain insights into human cognition.'
-					isFoundational={true}
-					reviewCount={150}
-					reviewValue={4}
-					difficulty={2.0}
-					image='/static/omshub-stub.jpg'
-				></ClassCard>
+				<ClassCard {...args}></ClassCard>
 			</Grid>
 		</Grid>
 	</>
 )
 
 export const Primary = Template.bind({})
+Primary.args = {
+	title: 'Knowledge Based AI',
+	acronym: 'KBAI',
+	classId: 'CS 7637',
+	blurb:
+		'The twin goals of knowledge-based artificial intelligence (AI) are to build AI agents capable of human-level intelligence and gain insights into human cognition.',
+	isFoundational: true,
+	reviewCount: 150,
+	reviewValue: 4,
+	difficulty: 2.0,
+	image: '/static/omshub-stub.jpg',
+}
