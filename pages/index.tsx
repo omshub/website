7e2901b69b40609ccf997a0f8c54dad246100892
Herman Rogers <herman.rogers@pm.me>
2022-05-17 17:22:05 -0400

    feat(readonly): create a readonly site for current students

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,33 @@
-import * as React from 'react'
+import React, { useState, useEffect } from 'react'
 import type { NextPage } from 'next'
 import Container from '@mui/material/Container'
 import Typography from '@mui/material/Typography'
 import Box from '@mui/material/Box'
+import Grid from '@mui/material/Grid'
 import Copyright from '../src/Copyright'
-import Link from '../src/Link'
+import ClassCard from '../src/components/ClassCard'
+
+interface ClassData {
+	number: string
+	aliases: string
+	department: string
+	deprecated: boolean
+	foundational: string
+	name: string
+	link: string
+}
 
 const Home: NextPage = () => {
+	const [classes, setClasses] = useState<[ClassData]>()
+
+	useEffect(() => {
+		let classes: [
+			ClassData
+		] = require('../public/static/data/omscentral_courses.json')
+
+		setClasses(classes)
+	})
+
 	return (
 		<Container maxWidth='lg'>
 			<Box
@@ -21,23 +42,25 @@ const Home: NextPage = () => {
 				<Typography variant='h4' component='h1' sx={{ mb: 10 }} gutterBottom>
 					OMSCS Course List
 				</Typography>
-				Hello! OMSHub is under construction. While the formal OMSHub website is
-				being developed, we wanted to offer the community a means of submitting
-				reviews in the interim period with a Google Form. All reviews posted to
-				this form will ultimately be loaded into the final website. In the mean
-				time, the results can be viewed on a spreadsheet.
-				<Link
-					href='https://docs.google.com/forms/d/e/1FAIpQLSc1xXBa3nnPECvoAKLMC4X3iXbZOghOiIQv6p8xAwR5gysBSA/viewform'
-					color='secondary'
-				>
-					Temporary Submission Form
-				</Link>
-				<Link
-					href='https://docs.google.com/spreadsheets/d/1ezCaFiye6dxVcdOCClndmz0JBLwMDrNtayL1WaTObj0/edit#gid=0'
-					color='secondary'
-				>
-					Temporary Spreadsheet With Reviews
-				</Link>
+				<Grid container spacing={3}>
+					{classes?.map((data, i) => (
+						<Grid key={i} item>
+							<ClassCard
+								title={data.name}
+								classId={data.number}
+								acronym={
+									JSON.parse(data.aliases).length > 0
+										? JSON.parse(data.aliases).toString().split(',').join(', ')
+										: ''
+								}
+								image='/static/omshub-stub.jpg'
+								isFoundational={data.foundational === 'true'}
+								department={data.department}
+								link={data.link}
+							></ClassCard>
+						</Grid>
+					))}
+				</Grid>
 			</Box>
 			<Copyright />
 		</Container>
--- src/components/ClassCard.tsx
+++ src/components/ClassCard.tsx
@@ -6,7 +6,7 @@ import Box from '@mui/material/Box'
 import Chip from '@mui/material/Chip'
 import Tooltip from '@mui/material/Tooltip'
 import Rating from '@mui/material/Rating'
-import { CardActionArea } from '@mui/material'
+import { CardActionArea, CardActions } from '@mui/material'
 import { grey } from '@mui/material/colors'
 import Typography from '@mui/material/Typography'
 
@@ -14,17 +14,19 @@ interface ClassCardProps {
 	title: string
 	acronym?: string
 	classId: string
-	blurb: string
+	blurb?: string
 	image?: string
 	isFoundational?: boolean
-	reviewCount: number
-	reviewValue: number
-	difficulty: number
+	reviewCount?: number
+	reviewValue?: number
+	difficulty?: number
+	department: string
+	link: string
 }
 
 const ClassCard = ({ ...props }: ClassCardProps) => {
 	return (
-		<Card sx={{ minWidth: 300, boxShadow: `0 5px 15px 0 ${grey[300]}` }}>
+		<Card sx={{ width: 350, boxShadow: `0 5px 15px 0 ${grey[300]}` }}>
 			<CardActionArea>
 				<CardMedia
 					component='img'
@@ -32,9 +34,9 @@ const ClassCard = ({ ...props }: ClassCardProps) => {
 					image={props.image}
 					alt='class background'
 				/>
-				<CardContent>
+				<CardContent sx={{ height: 150 }}>
 					<Typography variant='subtitle2' color='text.secondary' gutterBottom>
-						{props.acronym}
+						{props.department} {props.classId}
 					</Typography>
 					<Typography variant='h5' color='text.primary' gutterBottom>
 						{props.title}
@@ -42,69 +44,91 @@ const ClassCard = ({ ...props }: ClassCardProps) => {
 					<Typography noWrap variant='body2' color='text.secondary'>
 						{props.blurb}
 					</Typography>
-					<Box
-						sx={{
-							display: 'flex',
-							alignItems: 'center',
-							mt: 5,
-						}}
-					>
-						<Typography sx={{ mr: 1 }} variant='body2' color='text.secondary'>
-							{props.reviewCount} reviews
-						</Typography>
-						<Rating defaultValue={props.reviewValue} precision={0.5} readOnly />
-					</Box>
-					<Box
-						sx={{
-							display: 'flex',
-							alignItems: 'flex-start',
-							mt: 5,
-						}}
-					>
-						<Chip label={props.classId} variant='outlined' />
-						{(() => {
-							if (props.difficulty >= 4.0) {
-								return (
-									<Chip
-										label='Hard'
-										sx={{ ml: 0.5 }}
-										variant='outlined'
-										color='error'
-									></Chip>
-								)
-							} else if (props.difficulty >= 3.0) {
-								return (
-									<Chip
-										label='Medium'
-										sx={{ ml: 0.5 }}
-										variant='outlined'
-										color='warning'
-									></Chip>
-								)
-							} else {
-								return (
-									<Chip
-										label='Easy'
-										sx={{ ml: 0.5 }}
-										variant='outlined'
-										color='success'
-									></Chip>
-								)
-							}
-						})()}
-						{props.isFoundational ? (
-							<Tooltip title='This course satisfies the foundational requirement (first 2 courses with B or better).'>
-								<Chip
-									sx={{ ml: 0.5 }}
-									label='Foundational'
-									variant='outlined'
-									color='primary'
-								/>
-							</Tooltip>
-						) : null}
-					</Box>
 				</CardContent>
 			</CardActionArea>
+			<CardActions>
+				<Box
+					sx={{
+						display: 'flex',
+						alignItems: 'center',
+						mt: 5,
+					}}
+				>
+					{props.reviewValue && (
+						<>
+							<Typography sx={{ mr: 1 }} variant='body2' color='text.secondary'>
+								{props.reviewCount} reviews
+							</Typography>
+							<Rating
+								defaultValue={props.reviewValue}
+								precision={0.5}
+								readOnly
+							/>
+						</>
+					)}
+				</Box>
+				<Box
+					sx={{
+						display: 'flex',
+						alignItems: 'flex-start',
+						mt: 5,
+					}}
+				>
+					<Chip
+						label='Course Site'
+						component='a'
+						href={props.link}
+						variant='outlined'
+						clickable
+					/>
+					{props.acronym && (
+						<Chip label={props.acronym} sx={{ ml: 0.5 }} variant='outlined' />
+					)}
+					{(() => {
+						if (!props.difficulty) {
+							return <></>
+						}
+						if (props.difficulty >= 4.0) {
+							return (
+								<Chip
+									label='Hard'
+									sx={{ ml: 0.5 }}
+									variant='outlined'
+									color='error'
+								></Chip>
+							)
+						} else if (props.difficulty >= 3.0) {
+							return (
+								<Chip
+									label='Medium'
+									sx={{ ml: 0.5 }}
+									variant='outlined'
+									color='warning'
+								></Chip>
+							)
+						} else {
+							return (
+								<Chip
+									label='Easy'
+									sx={{ ml: 0.5 }}
+									variant='outlined'
+									color='success'
+								></Chip>
+							)
+						}
+					})()}
+					{props.isFoundational ? (
+						<Tooltip title='This course satisfies the foundational requirement (first 2 courses with B or better).'>
+							<Chip
+								sx={{ ml: 0.5 }}
+								label='Foundational'
+								variant='outlined'
+								color='primary'
+							/>
+						</Tooltip>
+					) : null}
+				</Box>
+			</CardActions>
 		</Card>
 	)
 }
