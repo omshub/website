df0a7efdf31cfd04e31c672b2f7653ba62f5769a
Herman Rogers <herman.rogers@pm.me>
2022-05-18 18:47:29 -0400

    refactor(loaders): add some loading states and cleanup app

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import Container from '@mui/material/Container'
 import Typography from '@mui/material/Typography'
 import Box from '@mui/material/Box'
 import Grid from '@mui/material/Grid'
+import CircularProgress from '@mui/material/CircularProgress'
 import ClassCard from '../src/components/ClassCard'
 
 interface ClassData {
@@ -18,14 +19,21 @@ interface ClassData {
 }
 
 const Home: NextPage = () => {
+	const [loading, setLoading] = useState<boolean>()
 	const [classes, setClasses] = useState<[ClassData]>()
 
 	useEffect(() => {
-		fetch(
-			"http://localhost:4000/classes"
-		).then((res) => res.json())
-		.then((classes) => setClasses(classes))
-		.catch(err => console.log(err))
+		setLoading(true)
+		fetch('http://localhost:4000/classes')
+			.then((res) => res.json())
+			.then((classes) => {
+				setLoading(false)
+				setClasses(classes)
+			})
+			.catch((err) => {
+				setLoading(false)
+				console.log(err)
+			})
 	}, [])
 
 	return (
@@ -40,8 +48,13 @@ const Home: NextPage = () => {
 				}}
 			>
 				<Typography variant='h4' component='h1' sx={{ mb: 10 }} gutterBottom>
-					OMSCS Course List
+					OMSCS Courses
 				</Typography>
+				{loading && (
+					<Box sx={{ display: 'flex', m: 10 }}>
+						<CircularProgress />
+					</Box>
+				)}
 				<Grid container spacing={3}>
 					{classes?.map((data, i) => (
 						<Grid key={i} item>
