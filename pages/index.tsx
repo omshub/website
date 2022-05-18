4f6b312d1ea0fcb03613c3d2f3547901f389dd63
csc5nz <ccabrera33@gatech.edu>
2022-05-18 04:37:53 +0000

    feat: feat: add links to Google form and spreadsheet
    fixed yarnk prettier
    
    feat #80

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,16 +21,21 @@ const Home: NextPage = () => {
 				<Typography variant='h4' component='h1' sx={{ mb: 10 }} gutterBottom>
 					OMSCS Course List
 				</Typography>
-				Hello OMSHub is under construction.
-				While the formal OMSHub website is being developed, we wanted to offer the community a means of submitting reviews 
-				in the interim period with a Google Form.
-				All reviews posted to this form will ultimately be loaded into the final website. In the mean time, the results 
-				can be viewed on a spreadsheet.
-				
-				<Link href='https://docs.google.com/forms/d/e/1FAIpQLSc1xXBa3nnPECvoAKLMC4X3iXbZOghOiIQv6p8xAwR5gysBSA/viewform' color='secondary'>
+				Hello OMSHub is under construction. While the formal OMSHub website is
+				being developed, we wanted to offer the community a means of submitting
+				reviews in the interim period with a Google Form. All reviews posted to
+				this form will ultimately be loaded into the final website. In the mean
+				time, the results can be viewed on a spreadsheet.
+				<Link
+					href='https://docs.google.com/forms/d/e/1FAIpQLSc1xXBa3nnPECvoAKLMC4X3iXbZOghOiIQv6p8xAwR5gysBSA/viewform'
+					color='secondary'
+				>
 					Temporary Submission Form
 				</Link>
-				<Link href='https://docs.google.com/spreadsheets/d/1ezCaFiye6dxVcdOCClndmz0JBLwMDrNtayL1WaTObj0/edit#gid=0' color='secondary'>
+				<Link
+					href='https://docs.google.com/spreadsheets/d/1ezCaFiye6dxVcdOCClndmz0JBLwMDrNtayL1WaTObj0/edit#gid=0'
+					color='secondary'
+				>
 					Temporary Spreadsheet With Reviews
 				</Link>
 			</Box>
