87bd8e49890d8da3e0afd9a96afdd1c14e7e36d5
Herman Rogers <herman.rogers@pm.me>
2022-05-18 18:47:29 -0400

    refactor(loaders): add some loading states and cleanup app

--- pages/class/[classid].tsx
+++ pages/class/[classid].tsx
@@ -5,18 +5,26 @@ import Container from '@mui/material/Container'
 import Typography from '@mui/material/Typography'
 import Box from '@mui/material/Box'
 import Grid from '@mui/material/Grid'
+import CircularProgress from '@mui/material/CircularProgress'
 import CommentCard from '../../src/components/CommentCard'
 
 const ClassID: NextPage = () => {
 	const router = useRouter()
-
+	const [loading, setLoading] = useState<boolean>()
 	const [reviews, setReviews] = useState<any>()
 	useEffect(() => {
+		setLoading(true)
 		if (router.isReady) {
 			fetch(`http://localhost:4000/reviews?id=${router.query.classid}`)
 				.then((response) => response.json())
-				.then((reviews) => setReviews(reviews))
-				.catch((err) => console.log(err))
+				.then((reviews) => {
+					setLoading(false)
+					setReviews(reviews)
+				})
+				.catch((err) => {
+					setLoading(false)
+					console.log(err)
+				})
 		}
 	}, [router.isReady])
 
@@ -34,6 +42,11 @@ const ClassID: NextPage = () => {
 				<Typography variant='h2' color='text.secondary' gutterBottom>
 					{router.query.title}
 				</Typography>
+				{loading && (
+					<Box sx={{ display: 'flex', m: 10 }}>
+						<CircularProgress />
+					</Box>
+				)}
 				<Grid container spacing={3}>
 					{reviews?.map((data: any, i: number) => (
 						<Grid key={i} item>
