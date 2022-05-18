f17d15fe3eb9c7c85ce14a43d331821e5eb2f771
Herman Rogers <herman.rogers@pm.me>
2022-05-18 19:31:19 -0400

    style(index page): cleanup the styles for the message

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,7 @@ import Box from '@mui/material/Box'
 import Grid from '@mui/material/Grid'
 import CircularProgress from '@mui/material/CircularProgress'
 import ClassCard from '../src/components/ClassCard'
+import Link from '../src/Link'
 
 interface ClassData {
 	number: string
@@ -47,7 +48,31 @@ const Home: NextPage = () => {
 					alignItems: 'center',
 				}}
 			>
-				<Typography variant='h4' component='h1' sx={{ mb: 10 }} gutterBottom>
+				<Typography variant='body1' sx={{ mb: 5 }}>
+					Hello! OMSHub is under construction. While the formal OMSHub website
+					is being developed, we wanted to offer the community a means of
+					submitting reviews in the interim period with a Google Form. All
+					reviews posted to this form will ultimately be loaded into the final
+					website. In the mean time, the results can be viewed on a spreadsheet.
+				</Typography>
+				<Link
+					href='https://docs.google.com/forms/d/e/1FAIpQLSc1xXBa3nnPECvoAKLMC4X3iXbZOghOiIQv6p8xAwR5gysBSA/viewform'
+					color='secondary'
+				>
+					Temporary Submission Form
+				</Link>
+				<Link
+					href='https://docs.google.com/spreadsheets/d/1ezCaFiye6dxVcdOCClndmz0JBLwMDrNtayL1WaTObj0/edit#gid=0'
+					color='secondary'
+				>
+					Temporary Spreadsheet With Reviews
+				</Link>
+				<Typography
+					variant='h4'
+					component='h1'
+					sx={{ mt: 5, mb: 10 }}
+					gutterBottom
+				>
 					OMSCS Courses
 				</Typography>
 				{loading && (
