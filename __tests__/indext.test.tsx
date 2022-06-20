c3a0447af157cc4dc0b12d6268f6f36a6c011ed9
Matthew Caseres <matthewcaseres@outlook.com>
2022-06-19 20:51:10 -0500

    feat(use ssg not fetch): use ssg not fetch

--- __tests__/indext.test.tsx
+++ __tests__/indext.test.tsx
@@ -1,12 +1,29 @@
 import Home from '@/pages/index'
 import { render, screen } from '@testing-library/react'
 import 'jest-fetch-mock'
+import { HomeProps } from '../pages/index'
 
 jest.mock('../src/Link')
 
+const props: HomeProps = {
+	classesWithId: [
+		{
+			number: '6035',
+			aliases: 'IIS',
+			department: 'Computer Science',
+			deprecated: false,
+			foundational: 'Foundational',
+			name: 'Introduction to Information Security',
+			link: 'https://omscs.gatech.edu/cs-6035-introduction-to-information-security',
+			course_id: 'CS-6035',
+			id: 1,
+		},
+	],
+}
+
 describe('Home', () => {
 	it('renders heading', () => {
-		render(<Home />)
+		render(<Home {...props} />)
 		expect(screen.getByRole('heading')).toHaveTextContent('OMSCS Courses')
 	})
 })
