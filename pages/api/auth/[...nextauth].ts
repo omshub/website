ee45dfb4f9aa7f6cbbcc30cf7965d1fa3f7d3729
Christian Tran <ctran4347@gmail.com>
2022-07-09 00:28:44 -0700

    feat(auth): added google auth, sign in, sign out, updated menu

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,44 +1,29 @@
-import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/google"
-import FacebookProvider from "next-auth/providers/facebook"
-import GithubProvider from "next-auth/providers/github"
-import TwitterProvider from "next-auth/providers/twitter"
-import Auth0Provider from "next-auth/providers/auth0"
-// import AppleProvider from "next-auth/providers/apple"
-// import EmailProvider from "next-auth/providers/email"
+import NextAuth from 'next-auth'
+import GoogleProvider from 'next-auth/providers/google'
 
 export default NextAuth({
-    // https://next-auth.js.org/configuration/providers/oauth
-  providers: [
-    FacebookProvider({
-      clientId: process.env.FACEBOOK_ID,
-      clientSecret: process.env.FACEBOOK_SECRET,
-    }),
-    GithubProvider({
-      clientId: process.env.GITHUB_CLIENT_ID,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    }),
-    GoogleProvider({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET,
-    }),
-    TwitterProvider({
-      clientId: process.env.TWITTER_ID,
-      clientSecret: process.env.TWITTER_SECRET,
-    }),
-    Auth0Provider({
-      clientId: process.env.AUTH0_ID,
-      clientSecret: process.env.AUTH0_SECRET,
-      issuer: process.env.AUTH0_ISSUER,
-    }),
-  ],
-  theme: {
-    colorScheme: "light",
-    logo: "/logo.png"
-  },
-  callbacks: {
-    async jwt({ token }) {
-      return token
-    }
-  },
+	providers: [
+		GoogleProvider({
+			clientId: process.env.GOOGLE_CLIENT_ID!,
+			clientSecret: process.env.GOOGLE_SECRET!,
+		}),
+	],
+	theme: {
+		colorScheme: 'light',
+		logo: '/logo.png',
+	},
+	// callbacks: {
+	// 	async signIn({ data }) {
+	// 		return true
+	// 	},
+	// 	async redirect({ url, baseUrl }) {
+	// 		return baseUrl
+	// 	},
+	// 	async session({ session, user, token }) {
+	// 		return session
+	// 	},
+	// 	async jwt({ token, user, account, profile, isNewUser }) {
+	// 		return token
+	// 	},
+	// },
 })
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -5,11 +5,62 @@ import { grey } from '@mui/material/colors'
 import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography'
 import Link from '../Link'
+import {  useSession } from "next-auth/react"
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import Box from '@mui/material/Box';
+import IconButton from '@mui/material/IconButton'
+import AccountCircle from '@mui/icons-material/AccountCircle';
 
 interface NavBarProps {}
 
 export const NavBar = ({ ...props }: NavBarProps) => {
+	const { status } = useSession()
+	const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+	
+	const isMenuOpen = Boolean(anchorEl);
+
+	const handleProfileMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
+		setAnchorEl(event.currentTarget);
+	};
+
+	const handleMenuClose = () => {
+		setAnchorEl(null);
+	};
+
+  	const menuId = 'primary-search-account-menu';
+	
+	const renderMenu = (
+		<>
+		<IconButton
+              size="large"
+              edge="end"
+              aria-label="account of current user"
+              aria-controls={menuId}
+              aria-haspopup="true"
+              onClick={handleProfileMenuOpen}
+              color="inherit"
+            >
+              <AccountCircle />
+		</IconButton>
+		<Menu
+		  anchorEl={anchorEl}
+		  anchorOrigin={{
+			vertical: 'bottom',
+			horizontal: 'right',
+		  }}
+		  id={menuId}
+		  keepMounted
+		  open={isMenuOpen}
+		  onClose={handleMenuClose}
+		>
+		  <MenuItem onClick={handleMenuClose}>My account</MenuItem>
+		  <MenuItem component={Link} href="/api/auth/signout">Logout</MenuItem>
+		</Menu>
+		</>
+	  );
 	return (
+		<Box sx={{ flexGrow: 1 }}>
 		<AppBar
 			position='static'
 			color='default'
@@ -41,17 +92,24 @@ export const NavBar = ({ ...props }: NavBarProps) => {
 						About
 					</Link>
 				</nav>
+				{ status !== 'authenticated' ? 
 				<Link
 						variant='button'
 						color='text.primary'
 						href='/api/auth/signin'
 						sx={{ my: 1, mx: 1.5 }}
 					>
-				<Button variant='outlined' sx={{ my: 1, mx: 1.5 }}>
-					Login
-				</Button>
+					<Button variant='outlined' sx={{ my: 1, mx: 1.5 }}>
+						Login
+					</Button>
 				</Link>
+				:
+				<Box sx={{ flexGrow: 0 }}>
+					{renderMenu}
+			  	</Box>
+			  }
 			</Toolbar>
 		</AppBar>
+		</Box>
 	)
 }
