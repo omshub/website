057b7c22f90f1d67d5edb9569b45eb4be1c86f94
awpala <apala7@gatech.edu>
2022-07-13 04:16:27 +0000

    feat: init firebase project

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
+
 module.exports = {
 	reactStrictMode: true,
+	env: {
+		baseUrl: process.env.BASE_URL,
+		apiKey: process.env.API_KEY,
+		authDomain: process.env.AUTH_DOMAIN,
+		projectId: process.env.PROJECT_ID,
+		storageBucket: process.env.STORAGE_BUCKET,
+		messagingSenderId: process.env.MESSAGING_SENDER_ID,
+		appId: process.env.APP_ID,
+		measurementId: process.env.MEASUREMENT_ID,
+	},
 }
