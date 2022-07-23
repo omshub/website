35c1737326fc4526ac12c590fa0bdcf0842a49a2
awpala <apala7@gatech.edu>
2022-07-23 18:24:20 +0000

    feat(db): add seed data for programs and specs

---
+++ firebase/seed/data/specializations.js
@@ -0,0 +1,26 @@
+const specializations = [
+	{ specializationId: 'a:at', name: 'Analytical Tools', programId: 'a' },
+	{ specializationId: 'a:ba', name: 'Business Analytics', programId: 'a' },
+	{
+		specializationId: 'a:cda',
+		name: 'Computational Data Analytics',
+		programId: 'a',
+	},
+	{
+		specializationId: 'cs:cpr',
+		name: 'Computational Perception & Robotics',
+		programId: 'cs',
+	},
+	{ specializationId: 'cs:cs', name: 'Computing Systems', programId: 'cs' },
+	{
+		specializationId: 'cs:ii',
+		name: 'Interactive Intelligence',
+		programId: 'cs',
+	},
+	{ specializationId: 'cs:ml', name: 'Machine Learning', programId: 'cs' },
+	{ specializationId: 'cy:es', name: 'Energy Systems', programId: 'cy' },
+	{ specializationId: 'cy:is', name: 'Information Security', programId: 'cy' },
+	{ specializationId: 'cy:pol', name: 'Policy', programId: 'cy' },
+]
+
+module.exports = { specializations }
