e426da8f15551a87ca08c42f8f2cbb5336b932d0
awpala <apala7@gatech.edu>
2022-08-06 16:07:59 +0000

    test(db): extract utility functions and add corresponding unit tests

--- firebase/utilities.ts
+++ firebase/utilities.ts
@@ -18,137 +18,7 @@ import {
 	TDocumentData,
 	TDocumentDataObject,
 } from './documentsDataTypes'
-
-/* --- UTILITY FUNCTIONS --- */
-const LEN_SIMPLE_COURSE_NUMBER = 5 //   DD-CCCC     (e.g., CS-6200)
-const LEN_COMPOUND_COURSE_NUMBER = 6 // DD-CCCC-CCC (e.g., CS-8803-O08)
-
-export const parseReviewId = (reviewId: string) => {
-	let courseId = ''
-	let departmentId = ''
-	let courseNumberA = ''
-	let courseNumberB = ''
-	let year = ''
-	let semesterTerm = ''
-
-	const parsedValues = reviewId.split('-')
-
-	if (parsedValues.length === LEN_SIMPLE_COURSE_NUMBER) {
-		;[departmentId, courseNumberA, year, semesterTerm] = parsedValues
-		courseId = `${departmentId}-${courseNumberA}`
-	}
-
-	if (parsedValues.length === LEN_COMPOUND_COURSE_NUMBER) {
-		;[departmentId, courseNumberA, courseNumberB, year, semesterTerm] =
-			parsedValues
-		courseId = `${departmentId}-${courseNumberA}-${courseNumberB}`
-	}
-
-	return {
-		courseId,
-		year,
-		semesterTerm,
-	}
-}
-
-type TAveragesData = {
-	oldAverage?: number | null
-	oldCount?: number
-	newCount: number
-	oldValue?: number | null
-	newValue?: number | null
-}
-
-export const updateAverage = ({
-	oldAverage,
-	oldCount = 0, // 0 for `oldAverage === null`
-	newCount,
-	oldValue = 0, // 0 for add new value
-	newValue = 0, // 0 for delete existing value
-}: TAveragesData) => {
-	if (newCount === 0) {
-		return null
-	}
-
-	oldAverage = oldAverage ?? 0
-	oldValue = oldValue ?? 0
-	newValue = newValue ?? 0
-
-	// adding new value:        newCount - oldCount ===  1
-	// editing existing value:  newCount - oldCount ===  0
-	// deleting existing value: newCount - oldCount === -1
-	return (
-		(oldAverage * oldCount - oldValue + newValue) /
-		(oldCount + (newCount - oldCount))
-	)
-}
-
-type TAveragesInputData = {
-	avgWorkload?: number | null
-	avgDifficulty?: number | null
-	avgOverall?: number | null
-	avgStaffSupport?: number | null
-	oldWorkload?: number
-	oldDifficulty?: number
-	oldOverall?: number
-	oldStaffSupport?: number | null
-	newWorkload?: number
-	newDifficulty?: number
-	newOverall?: number
-	newStaffSupport?: number | null
-	oldCount?: number
-	newCount: number
-	oldValue?: number | null
-	newValue?: number | null
-}
-
-// This function converts input averages to output averages based on
-// review add, edit, or delete
-const updateAverages = ({
-	oldCount,
-	newCount,
-	oldWorkload,
-	oldDifficulty,
-	oldOverall,
-	oldStaffSupport,
-	newWorkload,
-	newDifficulty,
-	newOverall,
-	newStaffSupport,
-	avgWorkload,
-	avgDifficulty,
-	avgOverall,
-	avgStaffSupport,
-}: TAveragesInputData) => ({
-	avgWorkload: updateAverage({
-		oldAverage: avgWorkload,
-		oldCount,
-		newCount,
-		oldValue: oldWorkload,
-		newValue: newWorkload,
-	}),
-	avgDifficulty: updateAverage({
-		oldAverage: avgDifficulty,
-		oldCount,
-		newCount,
-		oldValue: oldDifficulty,
-		newValue: newDifficulty,
-	}),
-	avgOverall: updateAverage({
-		oldAverage: avgOverall,
-		oldCount,
-		newCount,
-		oldValue: oldOverall,
-		newValue: newOverall,
-	}),
-	avgStaffSupport: updateAverage({
-		oldAverage: avgStaffSupport,
-		oldCount,
-		newCount,
-		oldValue: oldStaffSupport,
-		newValue: newStaffSupport,
-	}),
-})
+import { parseReviewId, updateAverages } from './utilityFunctions'
 
 /* --- CORE DATA CRUD SUB-OPERATIONS --- */
 export const getAll = async (dataDocName: string) => {
@@ -480,10 +350,9 @@ export const updateReviewsRecent50OnDeleteReview = async (reviewId: string) => {
 				.indexOf(reviewId)
 
 			if (indexFoundAt !== -1) {
-				dataArray = [
-					...dataArray.slice(0, indexFoundAt),
-					...dataArray.slice(indexFoundAt + 1),
-				]
+				dataArray = dataArray.filter(
+					(data: Review, index: number) => index !== indexFoundAt
+				)
 
 				if (dataArray?.length > 50) {
 					dataArray?.pop()
