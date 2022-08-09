3c770d20db141f148732c2d8f0768b10343936a2
awpala <apala7@gatech.edu>
2022-08-09 02:04:40 +0000

    feat(recents): init new components

---
+++ pages/recents.tsx
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from 'react'
+import type { NextPage } from 'next'
+import { Review } from '@globals/types'
+import Container from '@mui/material/Container'
+import { getReviewsRecent50 } from '@backend/dbOperations'
+import Grid from '@mui/material/Grid'
+import ReviewCard from '@components/ReviewCard'
+import Box from '@mui/material/Box'
+import CircularProgress from '@mui/material/CircularProgress'
+
+const Recents: NextPage = () => {
+	const [loading, setLoading] = useState<boolean>()
+	const [reviews, setReviews] = useState<Review[]>([])
+
+	useEffect(() => {
+		setLoading(true)
+
+		getReviewsRecent50()
+			.then((reviews) => {
+				if (reviews) {
+					setReviews(reviews)
+					setLoading(false)
+				}
+			})
+			.catch((err: any) => {
+				console.log(err)
+			})
+	}, [])
+
+	return (
+		<Container>
+			{loading ? (
+				<Box sx={{ display: 'flex', m: 10 }}>
+					<CircularProgress />
+				</Box>
+			) : (
+				<>
+					{reviews && (
+						<Grid container spacing={3} sx={{ margin: '10px 0' }}>
+							{reviews.slice(0, 50).map((value: Review) => {
+								return (
+									<Grid sx={{ width: `100%` }} key={value.reviewId} item>
+										<ReviewCard {...value}></ReviewCard>
+									</Grid>
+								)
+							})}
+						</Grid>
+					)}
+				</>
+			)}
+		</Container>
+	)
+}
+
+export default Recents
