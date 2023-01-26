f4e9ad30820bdd97ed5497569ebbca1c6615c7ea
awpala <apala7@gatech.edu>
2023-01-26 18:03:02 +0000

    fix: lint error

--- functions/src/dbConfig.ts
+++ functions/src/dbConfig.ts
@@ -1,5 +1,5 @@
 import * as admin from 'firebase-admin';
 
 // reference: https://firebase.google.com/docs/admin/setup
-const firebaseApp = admin.initializeApp()
+const firebaseApp = admin.initializeApp();
 export const dbAdmin = admin.firestore(firebaseApp);
