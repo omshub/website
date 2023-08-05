d8f82f61e2603fe21aea57129d96c3f3565bd821
awpala <apala7@gatech.edu>
2023-08-05 04:19:55 +0000

    test: add configs and mocks for `firebase`

--- jest.config.js
+++ jest.config.js
@@ -13,6 +13,11 @@ const customJestConfig = {
   moduleNameMapper: {
     // Handle module aliases (this will be automatically configured for you soon)
     '^@/pages/(.*)$': '<rootDir>/pages/$1',
+    '@backend/(.*)$': '<rootDir>/firebase/$1',
+    'firebase/app': '<rootDir>/firebase/__mocks__/fbApp.ts',
+    'firebase/auth': '<rootDir>/firebase/__mocks__/fbAuth.ts',
+    'firebase/firestore': '<rootDir>/firebase/__mocks__/fbFirestore.ts',
+    'firebase/storage': '<rootDir>/firebase/__mocks__/fbStorage.ts',
   },
   moduleDirectories: ['node_modules', '<rootDir>/'],
   testEnvironment: 'jest-environment-jsdom',
