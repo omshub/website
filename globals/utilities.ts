70ba001759972cb83bebabec8474c8ff4d593361
awpala <apala7@gatech.edu>
2023-08-06 03:09:48 +0000

    docs(`utilities.ts`): add jsdocs to function definitions

--- globals/utilities.ts
+++ globals/utilities.ts
@@ -55,12 +55,16 @@ export const getGrade = (gradeId: TGradeId) => grades[gradeId];
 
 /* --- MAPPERS --- */
 
+/**
+ * Converts Firebase Firestore DB payload to full `Course` data model
+ * @param coursesDataDynamic Response payload from Firebase Firestore DB
+ * @returns Complete `Course` data model as `TPayloadCourses`
+ */
 export const mapDynamicCoursesDataToCourses = (
   coursesDataDynamic: TPayloadCoursesDataDynamic,
 ) => {
   const coursesDataStatic = getCoursesDataStatic();
-  // @ts-ignore -- courses is populated in subsequent `forEach`
-  const courses: TPayloadCourses = {};
+  const courses = {} as TPayloadCourses;
   // @ts-ignore -- `TCourseId` is guaranteed by `coursesDataStatic`
   Object.keys(coursesDataStatic).forEach((courseId: TCourseId) => {
     courses[courseId] = {
@@ -73,14 +77,30 @@ export const mapDynamicCoursesDataToCourses = (
 
 /* --- UTILITY FUNCTIONS --- */
 
-export const isGTEmail = (userEmail: string) =>
-  userEmail
-    .toLowerCase()
-    .slice(userEmail.length - DOMAIN_GATECH.length)
-    .includes(DOMAIN_GATECH);
+/**
+ * Extract email domain from email input
+ * @param userEmail Input user email
+ * @param domain Input email domain (including prefix `@`)
+ * @returns Email domain
+ * @example
+ * // `gatechDomain` has value `@gatech.edu`
+ * const gatechDomain = extractEmailDomain('gpb@gatech.edu', '@gatech.edu');
+ */
+export const extractEmailDomain = (userEmail: string, domain: string) =>
+  userEmail.toLowerCase().slice(userEmail.length - domain.length);
 
+/**
+ * Determine if `userEmail` has domain `@gatech.edu`
+ * @param userEmail Input user email
+ * @returns Boolean `true` (domain is `@gatech.edu`) or `false` (otherwise)
+ */
+export const isGTEmail = (userEmail: string) =>
+  extractEmailDomain(userEmail, DOMAIN_GATECH).includes(DOMAIN_GATECH);
+
+/**
+ * Determine if `userEmail` has domain `@outlook.com`
+ * @param userEmail Input user email
+ * @returns Boolean `true` (domain is `@outlook.com`) or `false` (otherwise)
+ */
 export const isOutlookEmail = (userEmail: string) =>
-  userEmail
-    .toLowerCase()
-    .slice(userEmail.length - DOMAIN_OUTLOOK.length)
-    .includes(DOMAIN_OUTLOOK);
+  extractEmailDomain(userEmail, DOMAIN_OUTLOOK).includes(DOMAIN_OUTLOOK);
