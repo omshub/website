6cbc3f255fa5e1071fc400dba1098eca4f6d1eb2
awpala <apala7@gatech.edu>
2023-08-06 16:51:16 +0000

    chore(`utilities.test.ts`): reorganize tests into arrange-act-assert

--- __tests__/utilities.test.ts
+++ __tests__/utilities.test.ts
@@ -43,32 +43,32 @@ describe('frontend utilities tests', () => {
     });
 
     it('maps payload to array, ascending by sortKey', () => {
+      const mappedArray = mapPayloadToArray(inputPayload, 'sortKeyField');
       const expectedOutput = [
         { sortKeyField: 1, courseId: 'CS-6150' },
         { sortKeyField: 2, courseId: 'CS-6200' },
         { sortKeyField: 3, courseId: 'CS-6035' },
       ];
-      const mappedArray = mapPayloadToArray(inputPayload, 'sortKeyField');
       expect(mappedArray).toMatchObject(expectedOutput);
     });
 
     it('maps payload to array, descending by sortKey', () => {
+      const mappedArray = mapPayloadToArray(inputPayload, 'courseID', DESC);
       const expectedOutput = [
         { sortKeyField: 2, courseId: 'CS-6200' },
         { sortKeyField: 1, courseId: 'CS-6150' },
         { sortKeyField: 3, courseId: 'CS-6035' },
       ];
-      const mappedArray = mapPayloadToArray(inputPayload, 'courseID', DESC);
       expect(mappedArray).toMatchObject(expectedOutput);
     });
 
     it('falls through in original order if no sortKey specified', () => {
+      const mappedArray = mapPayloadToArray(inputPayload);
       const expectedOutput = [
         { sortKeyField: 3, courseId: 'CS-6035' },
         { sortKeyField: 1, courseId: 'CS-6150' },
         { sortKeyField: 2, courseId: 'CS-6200' },
       ];
-      const mappedArray = mapPayloadToArray(inputPayload);
       expect(mappedArray).toMatchObject(expectedOutput);
     });
   });
