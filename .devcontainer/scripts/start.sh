893766b509bac863123d8d2dda6da903856cfd0a
Christian Tran <ctran4347@gmail.com>
2023-08-09 16:48:33 +0000

    fix: minor configuration change for dev container and update to script

--- .devcontainer/scripts/start.sh
+++ .devcontainer/scripts/start.sh
@@ -2,9 +2,6 @@
 
 # Performs setup tasks _after_ the .devcontainer Docker container is created.
 
-# Change from readonly access in workspace
-sudo chmod -R 777 .
-
 # Install Node dependencies.
 sudo yarn install
 sudo yarn build
