5c6515db6fd38ed39d645233e2b07fdb6de32ab3
Christian Tran <ctran4347@gmail.com>
2024-08-14 21:48:35 -0500

    Fix dev container's outdated node version (#520)
    * chore: upgrade to nextjs 14 and node 20
    
    * fix: node 20 for github workflow chromatic
    
    * fix(changing nodejs version for workflows): workflow update
    
    * chore(devcontainer): add labels for ports
    
    * fix(package.json): lock node20 and update dependencies
    
    ---------
    
    Co-authored-by: awpala <adam.w.pala@gmail.com>

--- next.config.js
+++ next.config.js
@@ -16,48 +16,4 @@ module.exports = {
     removeConsole: true,
   },
   transpilePackages: ['@mui/system', '@mui/material', '@mui/icons-material','@mui/styles'],
-  modularizeImports: {
-    '@mui/material/?(((\\w*)?/?)*)': {
-      transform: '@mui/material/{{ matches.[1] }}/{{member}}',
-    },
-    '@mui/icons-material/?(((\\w*)?/?)*)': {
-      transform: '@mui/icons-material/{{ matches.[1] }}/{{member}}',
-    },
-    '@mui/styles/?(((\\w*)?/?)*)':{
-      transform: '@mui/styles/{{ matches.[1] }}/{{member}}'
-    },
-    async headers() {
-      return [
-        {
-          source: '/:path*',
-          headers: [
-            {
-              key: 'X-DNS-Prefetch-Control',
-              value: 'on',
-            },
-            {
-              key: 'Strict-Transport-Security',
-              value: 'max-age=63072000; includeSubDomains; preload',
-            },
-            {
-              key: 'X-XSS-Protection',
-              value: '1; mode=block',
-            },
-            {
-              key: 'X-Frame-Options',
-              value: 'SAMEORIGIN',
-            },
-            {
-              key: 'X-Content-Type-Options',
-              value: 'nosniff',
-            },
-            {
-              key: 'Referrer-Policy',
-              value: 'origin-when-cross-origin',
-            },
-          ],
-        },
-      ];
-    },
-  },
 };
