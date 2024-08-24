4d9a34fab8aebe3a6c4bf159ca4744db0b898fe4
awpala <adam.w.pala@gmail.com>
2024-08-24 03:11:05 +0000

    feat(supabase): init tables, functions, and service

---
+++ services/backend.ts
@@ -0,0 +1,358 @@
+import { createClient } from '@supabase/supabase-js';
+import { TCourseId, Review, TSemesterId, TNullable, Course, CourseDataStatic } from '@globals/types';
+import { coursesDataStatic } from '@globals/staticDataModels';
+import { TDatabase } from '../supabase/types';
+import { mapPayloadToArray } from '@src/utilities';
+
+const supabase = createClient<TDatabase>(
+  process.env.SUPABASE_URL!,
+  process.env.SUPABASE_ANON_KEY!,
+);
+
+/* --- UTILITY FUNCTIONS ---*/
+
+/**
+ * Utility function to convert anonymous return type from Supabase API call to idiomatic global type `Review[]`.
+ * @param data Review data returned from Supabase API 
+ */
+const unpackReviewData = (data: Review[]): Review[] => {
+  const reviews: Review[] = [];
+
+  if (data.length) {
+    data.forEach(({
+      reviewId,
+      courseId,
+      year,
+      semesterId,
+      isLegacy,
+      reviewerId,
+      isGTVerifiedReviewer,
+      created,
+      modified,
+      body,
+      upvotes,
+      downvotes,
+      workload,
+      difficulty,
+      overall,
+    }) => {
+      reviews.push({
+        reviewId,
+        courseId,
+        year,
+        semesterId,
+        isLegacy,
+        reviewerId,
+        isGTVerifiedReviewer,
+        created,
+        modified,
+        body,
+        upvotes,
+        downvotes,
+        workload,
+        difficulty,
+        overall,
+      });
+    });
+  }
+
+  return reviews;
+};
+
+interface TCourseStats {
+  courseId: TCourseId;
+  numReviews: number;
+  year?: number;
+  semesterId?: TSemesterId;
+  avgWorkload: number;
+  avgDifficulty: number;
+  avgOverall: number;
+}
+
+const semesters: TSemesterId[] = ['sp', 'sm', 'fa'];
+
+// array of years `[<EARLIEST_REVIEW_YEAR>, ..., <currentYear>]`
+const years: number[] = (function() {
+  const EARLIEST_REVIEW_YEAR = 2015;
+  const currentYear = new Date().getFullYear();
+
+  const yearsList: number[] = [];
+
+  for (let yearItem = EARLIEST_REVIEW_YEAR; yearItem <= currentYear; yearItem++) {
+    yearsList.push(yearItem);
+  }
+
+  return yearsList;
+}());
+
+/**
+ * Utility function to convert anonymous return type from Supabase API call to idiomatic global type `Course[]`.
+ * @param stats Course statistics for either course aggregate (all years & semesters combined) or course-year-semester (individually).
+ * @returns {Course[]}
+ */
+const zipToCoursesData = (stats: TCourseStats[]): Course[] => {
+  const courses: Course[] = [];
+
+  const dataStatic: CourseDataStatic[] = mapPayloadToArray(coursesDataStatic);
+
+  const unreviewedCourse: Partial<TCourseStats> = {
+    numReviews: 0,
+    avgWorkload: 0,
+    avgDifficulty: 0,
+    avgOverall: 0,
+  };
+
+  years.forEach(y => {
+    semesters.forEach(s => {
+      dataStatic.forEach(({
+        courseId,
+        name,
+        departmentId,
+        courseNumber,
+        url,
+        aliases,
+        isDeprecated,
+        isFoundational,
+      }) => {
+        // aggregate course stats only (no year or semesterId)
+        const matchingStatsAggregate = stats.find(stat => stat.courseId === courseId && !stat?.year && !stat?.semesterId);
+        if (matchingStatsAggregate) {
+          courses.push({
+            courseId,
+            name,
+            departmentId,
+            courseNumber,
+            url,
+            aliases,
+            isDeprecated,
+            isFoundational,
+            numReviews: matchingStatsAggregate?.numReviews ?? unreviewedCourse.numReviews,
+            avgWorkload: matchingStatsAggregate?.avgWorkload ?? unreviewedCourse.avgWorkload,
+            avgDifficulty: matchingStatsAggregate?.avgDifficulty ?? unreviewedCourse.avgDifficulty,
+            avgOverall: matchingStatsAggregate?.avgOverall ?? unreviewedCourse.avgOverall,
+            avgStaffSupport: null,
+            reviewsCountsByYearSem: {}, // NOTE: placeholder only; remove once migrated from Firebase to Supabase
+          });
+
+          // exit inner loop prematurely if not returning year-semesterId data
+          return;
+        }
+
+        // remaining cases: courseId-year-SemesterId
+        const matchingStatsYearSemester = stats.find(stat => stat.courseId === courseId && stat.year == y && stat.semesterId == s);
+        courses.push({
+          courseId,
+          name,
+          departmentId,
+          courseNumber,
+          url,
+          aliases,
+          isDeprecated,
+          isFoundational,
+          year: matchingStatsYearSemester?.year ?? y!,
+          semesterId: matchingStatsYearSemester?.semesterId ?? s!,
+          numReviews: matchingStatsYearSemester?.numReviews ?? unreviewedCourse.numReviews!,
+          avgWorkload: matchingStatsYearSemester?.avgWorkload ?? unreviewedCourse.avgWorkload!,
+          avgDifficulty: matchingStatsYearSemester?.avgDifficulty ?? unreviewedCourse.avgDifficulty!,
+          avgOverall: matchingStatsYearSemester?.avgOverall ?? unreviewedCourse.avgOverall!,
+          avgStaffSupport: null,
+          reviewsCountsByYearSem: {}, // NOTE: placeholder only; remove once migrated from Firebase to Supabase
+        });
+      });
+    });
+  });
+
+  return courses;
+}
+
+/* --- SUPABASE FUNCTIONS: READ OPERATIONS ---*/
+
+/**
+ * Get rows from Supabase table of type `Review`, returned with most recent reviews first.
+ * @param limitCount Limit of rows returned. Default is defined as `DEFAULT_LIMIT` inside function body.
+ */
+const getAllReviews = async (limitCount?: number): Promise<Review[]> => {
+  const DEFAULT_LIMIT = 50; // prevent large read of DB if argument is omitted
+  limitCount = limitCount ?? DEFAULT_LIMIT;
+
+  try {
+    const { data, error } = await supabase
+      .rpc('getAllReviews', {
+        limit_count: limitCount,
+      });
+
+    if (error) throw error;
+
+    let reviews: Review[] = [];
+
+    if (data && data.length) {
+      reviews = unpackReviewData(data as Review[]);
+    }
+
+    return reviews;
+  } catch (error: any) {
+    console.error(error);
+    return [];
+  }
+}
+
+/**
+ * Get a single review from Supabase table of type `Review`.
+ * @param reviewId The review ID, of general form `courseId-year-semesterId-created`.
+ * @returns {Review} 
+ */
+const getReviewByReviewId = async (reviewId: string): Promise<TNullable<Review>> => {
+  try {
+    const { data, error } = await supabase
+      .rpc('getReviewByReviewId', {
+        review_id: reviewId,
+      });
+
+    if (error) throw error;
+
+    const review: TNullable<Review> = data
+      ? data[0] as Review
+      : null;
+
+    return review;
+  } catch (error: any) {
+    console.error(error);
+    return null;
+  }
+}
+
+/**
+ * Get rows from Supabase table of type `Review` for a course, returned with most recent reviews first.
+ * @param courseId The OMSCS course ID. If omitted, returns all courses' reviews.
+ * @returns {Review[]} 
+ */
+const getReviewsByCourseId = async (courseId?: TCourseId): Promise<Review[]> => {
+  try {
+    const { data, error } = await supabase
+      .rpc('getReviewsByCourseId', {
+        course_id: courseId,
+      });
+
+    if (error) throw error;
+
+    let reviews: Review[] = [];
+
+    if (data && data.length) {
+      reviews = unpackReviewData(data as Review[]);
+    }
+
+    return reviews;
+  } catch (error: any) {
+    console.error(error);
+    return [];
+  }
+}
+
+/**
+ * Get rows from Supabase table of type `Review` for a user, returned with most recent reviews first.
+ * @param userId The Supabase Auth user ID.
+ * @returns {Review[]}
+ */
+const getReviewsByUserId = async (userId: string): Promise<Review[]> => {
+  try {
+    const { data, error } = await supabase
+      .rpc('getReviewsByUserId', {
+        user_id: userId,
+      });
+
+    if (error) throw error;
+
+    let reviews: Review[] = [];
+
+    if (data && data.length) {
+      reviews = unpackReviewData(data as Review[]);
+    }
+
+    return reviews;
+  } catch (error: any) {
+    console.error(error);
+    return [];
+  }
+}
+
+/**
+ * Get aggregate data from Supabase table of type `Course` for a course. Output includes zero-row data.
+ * @param courseId The OMSCS course ID. If omitted, returns all courses' stats (aggregated).
+ * @returns {Course[]}
+ */
+const getStatsByCourseId = async (courseId?: TCourseId): Promise<Course[]> => {
+  try {
+    const { data, error } = await supabase
+      .rpc('getStatsByCourseId', {
+        course_id: courseId,
+      });
+
+    if (error) throw error;
+
+    let courses: Course[] = [];
+
+    if (data && data.length) {
+      courses = zipToCoursesData(data as TCourseStats[]);
+    }
+
+    return courses;
+  } catch (error: any) {
+    console.error(error);
+    return [];
+  }
+}
+
+interface TArgsCourseStats {
+  courseId?: TCourseId;
+  year?: number;
+  semesterId?: TSemesterId;
+}
+
+/**
+ * Get aggregate data from Supabase table of type `Course` for a course. Output includes zero-row data (i.e., `numReviews == 0`). Can be queried independently for course and/or year and/or semester.
+ * @param courseId The OMSCS course ID. If omitted, returns all courses' stats (year-semesterId row-wise data).
+ * @param year The year of the course. If omitted, returns courseId and/or semesterId row-wise data.
+ * @param semesterId The semester of the course. If omitted, returns courseId and/or year row-wise data.
+ * @returns {Course[]}
+ */
+const getStatsByCourseYearSemester = async ({
+  courseId,
+  year,
+  semesterId,
+}: TArgsCourseStats): Promise<Course[]> => {
+  try {
+    const { data, error } = await supabase
+      .rpc('getStatsByCourseYearSemester', {
+        course_id: courseId,
+        year_: year,
+        semester_id: semesterId,
+      });
+
+    if (error) throw error;
+
+    let courses: Course[] = [];
+
+    if (data && data.length) {
+      courses = zipToCoursesData(data as TCourseStats[]);
+    }
+
+    return courses;
+  } catch (error: any) {
+    console.error(error);
+    return [];
+  }
+}
+
+/* --- EXPORTS --- */
+
+const backend = {
+  // READ OPERATIONS
+  getAllReviews,
+  getReviewByReviewId,
+  getReviewsByCourseId,
+  getReviewsByUserId,
+  getStatsByCourseId,
+  getStatsByCourseYearSemester,
+};
+
+export default backend;
---
+++ supabase/seed/functions.sql
@@ -0,0 +1,272 @@
+/* --- READ OPERATIONS --- */
+
+CREATE OR REPLACE FUNCTION "getAllReviews"(limit_count int DEFAULT NULL)
+RETURNS TABLE(
+    "reviewId" text,
+    "courseId" text,
+    "year" numeric,
+    "semesterId" text,
+    "isLegacy" boolean,
+    "reviewerId" text,
+    "isGTVerifiedReviewer" boolean,
+    created numeric,
+    modified numeric,
+    workload numeric,
+    difficulty numeric,
+    overall numeric,
+    upvotes numeric,
+    downvotes numeric,
+    body text
+) AS $$
+BEGIN
+    IF limit_count IS NULL THEN
+        RETURN QUERY
+        SELECT 
+            r."reviewId",
+            r."courseId",
+            r."year",
+            r."semesterId",
+            r."isLegacy",
+            r."reviewerId",
+            r."isGTVerifiedReviewer",
+            r.created,
+            r.modified,
+            r.workload,
+            r.difficulty,
+            r.overall,
+            r.upvotes,
+            r.downvotes,
+            r.body
+        FROM review r
+        ORDER BY created DESC;
+    ELSE
+        RETURN QUERY
+        SELECT 
+            r."reviewId",
+            r."courseId",
+            r."year",
+            r."semesterId",
+            r."isLegacy",
+            r."reviewerId",
+            r."isGTVerifiedReviewer",
+            r.created,
+            r.modified,
+            r.workload,
+            r.difficulty,
+            r.overall,
+            r.upvotes,
+            r.downvotes,
+            r.body
+        FROM review r
+        ORDER BY created DESC
+        LIMIT limit_count;
+    END IF;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION "getReviewByReviewId"(review_id text)
+RETURNS TABLE(
+    "reviewId" text,
+    "courseId" text,
+    "year" numeric,
+    "semesterId" text,
+    "isLegacy" boolean,
+    "reviewerId" text,
+    "isGTVerifiedReviewer" boolean,
+    created numeric,
+    modified numeric,
+    workload numeric,
+    difficulty numeric,
+    overall numeric,
+    upvotes numeric,
+    downvotes numeric,
+    body text
+) AS $$
+BEGIN
+    RETURN QUERY
+    SELECT
+        r."reviewId",
+        r."courseId",
+        r."year",
+        r."semesterId",
+        r."isLegacy",
+        r."reviewerId",
+        r."isGTVerifiedReviewer",
+        r.created,
+        r.modified,
+        r.workload,
+        r.difficulty,
+        r.overall,
+        r.upvotes,
+        r.downvotes,
+        r.body
+    FROM review r
+    WHERE r."reviewId" = review_id;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION "getReviewsByCourseId"(course_id text DEFAULT NULL)
+RETURNS TABLE(
+    "reviewId" text,
+    "courseId" text,
+    "year" numeric,
+    "semesterId" text,
+    "isLegacy" boolean,
+    "reviewerId" text,
+    "isGTVerifiedReviewer" boolean,
+    created numeric,
+    modified numeric,
+    workload numeric,
+    difficulty numeric,
+    overall numeric,
+    upvotes numeric,
+    downvotes numeric,
+    body text
+) AS $$
+BEGIN
+    RETURN QUERY
+    SELECT
+        r."reviewId",
+        r."courseId",
+        r."year",
+        r."semesterId",
+        r."isLegacy",
+        r."reviewerId",
+        r."isGTVerifiedReviewer",
+        r.created,
+        r.modified,
+        r.workload,
+        r.difficulty,
+        r.overall,
+        r.upvotes,
+        r.downvotes,
+        r.body
+    FROM review r
+    WHERE r."courseId" = COALESCE(course_id, r."courseId")
+    ORDER BY
+        r."year" DESC,
+        -- order semesters chronologically descending within a given year
+        CASE r."semesterId"
+            WHEN 'sp' THEN 3
+            WHEN 'sm' THEN 2
+            WHEN 'fa' THEN 1
+        END,
+        created DESC;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION "getReviewsByUserId"(user_id text)
+RETURNS TABLE(
+    "reviewId" text,
+    "courseId" text,
+    "year" numeric,
+    "semesterId" text,
+    "isLegacy" boolean,
+    "reviewerId" text,
+    "isGTVerifiedReviewer" boolean,
+    created numeric,
+    modified numeric,
+    workload numeric,
+    difficulty numeric,
+    overall numeric,
+    upvotes numeric,
+    downvotes numeric,
+    body text
+) AS $$
+BEGIN
+    RETURN QUERY
+    SELECT
+        r."reviewId",
+        r."courseId",
+        r."year",
+        r."semesterId",
+        r."isLegacy",
+        r."reviewerId",
+        r."isGTVerifiedReviewer",
+        r.created,
+        r.modified,
+        r.workload,
+        r.difficulty,
+        r.overall,
+        r.upvotes,
+        r.downvotes,
+        r.body
+    FROM review r
+    WHERE r."reviewerId" = user_id
+    ORDER BY
+        "year" DESC,
+        -- order semesters chronologically descending within a given year
+        CASE r."semesterId"
+            WHEN 'sp' THEN 3
+            WHEN 'sm' THEN 2
+            WHEN 'fa' THEN 1
+        END,
+        created DESC;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION "getStatsByCourseId"(course_id text DEFAULT NULL)
+RETURNS TABLE (
+    "courseId" text,
+    "numReviews" bigint,
+    "avgWorkload" numeric,
+    "avgDifficulty" numeric,
+    "avgOverall" numeric
+)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+RETURN QUERY
+SELECT 
+    r."courseId",
+    COUNT(*) AS "numReviews",
+    AVG(r.workload) AS "avgWorkload",
+    AVG(r.difficulty) AS "avgDifficulty",
+    AVG(r.overall) AS "avgOverall"
+FROM review r
+WHERE r."courseId" = COALESCE(course_id, r."courseId")
+GROUP BY r."courseId"
+ORDER BY r."courseId";
+END;
+$$;
+
+CREATE OR REPLACE FUNCTION "getStatsByCourseYearSemester"(course_id text DEFAULT NULL, year_ numeric DEFAULT NULL, semester_id text DEFAULT NULL)
+RETURNS TABLE (
+    "courseId" text,
+    "year" numeric,
+    "semesterId" text,
+    "numReviews" bigint,
+    "avgWorkload" numeric,
+    "avgDifficulty" numeric,
+    "avgOverall" numeric
+)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+RETURN QUERY
+SELECT 
+    r."courseId",
+    r."year",
+    r."semesterId",
+    COUNT(*) AS "numReviews",
+    AVG(r.workload) AS "avgWorkload",
+    AVG(r.difficulty) AS "avgDifficulty",
+    AVG(r.overall) AS "avgOverall"
+FROM review r
+WHERE (
+    r."courseId" = COALESCE(course_id, r."courseId")
+    AND r."year" = COALESCE(year_, r."year")
+    AND r."semesterId" = COALESCE(semester_id, r."semesterId")
+)
+GROUP BY r."courseId", r.year, r."semesterId"
+ORDER BY 
+    r."courseId",
+    r."year",
+    -- Order semesters chronologically ascending within a given year
+    CASE r."semesterId"
+        WHEN 'sp' THEN 1
+        WHEN 'sm' THEN 2
+        WHEN 'fa' THEN 3
+    END;
+END;
+$$;
