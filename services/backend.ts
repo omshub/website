a03476ba33ed909ac34f77d3263d0eaf815316a8
awpala <adam.w.pala@gmail.com>
2024-08-25 02:52:50 +0000

    refactor(services): extract utilities

--- services/backend.ts
+++ services/backend.ts
@@ -1,170 +1,13 @@
 import { createClient } from '@supabase/supabase-js';
-import { TCourseId, Review, TSemesterId, TNullable, Course, CourseDataStatic } from '@globals/types';
-import { coursesDataStatic } from '@globals/staticDataModels';
+import { TCourseId, Review, TSemesterId, TNullable, Course } from '@globals/types';
 import { TDatabase } from '../supabase/types';
-import { mapPayloadToArray } from '@src/utilities';
-import { courseFields } from '@globals/constants';
+import { unpackReviewData, zipToCoursesData } from './utilities/supabaseData';
+import { TCourseStats } from './utilities/supabaseTypes';
 
 const supabase = createClient<TDatabase>(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
 );
-
-/* --- UTILITY FUNCTIONS ---*/
-
-/**
- * Utility function to convert anonymous return type from Supabase API call to idiomatic global type `Review[]`.
- * @param data Review data returned from Supabase API 
- */
-const unpackReviewData = (data: Review[]): Review[] => {
-  const reviews: Review[] = [];
-
-  if (data.length) {
-    data.forEach(({
-      reviewId,
-      courseId,
-      year,
-      semesterId,
-      isLegacy,
-      reviewerId,
-      isGTVerifiedReviewer,
-      created,
-      modified,
-      body,
-      upvotes,
-      downvotes,
-      workload,
-      difficulty,
-      overall,
-    }) => {
-      reviews.push({
-        reviewId,
-        courseId,
-        year,
-        semesterId,
-        isLegacy,
-        reviewerId,
-        isGTVerifiedReviewer,
-        created,
-        modified,
-        body,
-        upvotes,
-        downvotes,
-        workload,
-        difficulty,
-        overall,
-      });
-    });
-  }
-
-  return reviews;
-};
-
-interface TCourseStats {
-  courseId: TCourseId;
-  numReviews: number;
-  year?: number;
-  semesterId?: TSemesterId;
-  avgWorkload: TNullable<number>;
-  avgDifficulty: TNullable<number>;
-  avgOverall: TNullable<number>;
-}
-
-const semesters: TSemesterId[] = ['sp', 'sm', 'fa'];
-
-// array of years `[<EARLIEST_REVIEW_YEAR>, ..., <currentYear>]`
-const years: number[] = (function() {
-  const EARLIEST_REVIEW_YEAR = 2015;
-  const currentYear = new Date().getFullYear();
-
-  const yearsList: number[] = [];
-
-  for (let yearItem = EARLIEST_REVIEW_YEAR; yearItem <= currentYear; yearItem++) {
-    yearsList.push(yearItem);
-  }
-
-  return yearsList;
-}());
-
-/**
- * Utility function to convert anonymous return type from Supabase API call to idiomatic global type `Course[]`.
- * @param stats Course statistics for either course aggregate (all years & semesters combined) or course-year-semester (individually).
- * @returns {Course[]}
- */
-const zipToCoursesData = (stats: TCourseStats[]): Course[] => {
-  const courses: Course[] = [];
-
-  const dataStatic: CourseDataStatic[] = mapPayloadToArray(coursesDataStatic, courseFields.courseId);
-
-  const unreviewedCourse: Partial<TCourseStats> = {
-    numReviews: 0,
-    avgWorkload: null,
-    avgDifficulty: null,
-    avgOverall: null,
-  };
-
-  years.forEach(y => {
-    semesters.forEach(s => {
-      dataStatic.forEach(({
-        courseId,
-        name,
-        departmentId,
-        courseNumber,
-        url,
-        aliases,
-        isDeprecated,
-        isFoundational,
-      }) => {
-        // aggregate course stats only (no year or semesterId)
-        const matchingStatsAggregate = stats.find(stat => stat.courseId === courseId && !stat?.year && !stat?.semesterId);
-        if (matchingStatsAggregate) {
-          courses.push({
-            courseId,
-            name,
-            departmentId,
-            courseNumber,
-            url,
-            aliases,
-            isDeprecated,
-            isFoundational,
-            numReviews: (matchingStatsAggregate?.numReviews ?? unreviewedCourse.numReviews)!,
-            avgWorkload: (matchingStatsAggregate?.avgWorkload ?? unreviewedCourse.avgWorkload)!,
-            avgDifficulty: (matchingStatsAggregate?.avgDifficulty ?? unreviewedCourse.avgDifficulty)!,
-            avgOverall: (matchingStatsAggregate?.avgOverall ?? unreviewedCourse.avgOverall)!,
-            avgStaffSupport: null,
-            reviewsCountsByYearSem: {}, // NOTE: placeholder only; remove once migrated from Firebase to Supabase
-          });
-
-          // exit inner loop prematurely if not returning year-semesterId data
-          return;
-        }
-
-        // remaining cases: courseId-year-SemesterId
-        const matchingStatsYearSemester = stats.find(stat => stat.courseId === courseId && stat.year == y && stat.semesterId == s);
-        courses.push({
-          courseId,
-          name,
-          departmentId,
-          courseNumber,
-          url,
-          aliases,
-          isDeprecated,
-          isFoundational,
-          year: (matchingStatsYearSemester?.year ?? y)!,
-          semesterId: (matchingStatsYearSemester?.semesterId ?? s)!,
-          numReviews: (matchingStatsYearSemester?.numReviews ?? unreviewedCourse.numReviews)!,
-          avgWorkload: (matchingStatsYearSemester?.avgWorkload ?? unreviewedCourse.avgWorkload)!,
-          avgDifficulty: (matchingStatsYearSemester?.avgDifficulty ?? unreviewedCourse.avgDifficulty)!,
-          avgOverall: (matchingStatsYearSemester?.avgOverall ?? unreviewedCourse.avgOverall)!,
-          avgStaffSupport: null,
-          reviewsCountsByYearSem: {}, // NOTE: placeholder only; remove once migrated from Firebase to Supabase
-        });
-      });
-    });
-  });
-
-  return courses;
-}
 
 /* --- SUPABASE FUNCTIONS: READ OPERATIONS ---*/
 
@@ -345,6 +188,8 @@ const getStatsByCourseYearSemester = async ({
 }
 
 /* --- EXPORTS --- */
+
+// TODO: Add remaining operations (CREATE, UPDATE, DELETE)
 
 const backend = {
   // READ OPERATIONS
