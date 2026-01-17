3a222b967b9153de8a898567d3a21c44ed663f78
Christian <ctran4347@gmail.com>
2026-01-17 14:23:14 -0600

    chore(publishable key): switch anon key for publishable key

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -4,7 +4,7 @@ import type { Database } from './database.types';
 export function createClient() {
   return createBrowserClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!
   );
 }
 
